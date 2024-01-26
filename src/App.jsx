1686c539f43b7c82cc70e40e71439ced6332bf44
Young <youngheesomemoto@gmail.com>
2024-01-25 21:18:55 -0600

    colored dots

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 // Write your Color component here
 
+const Color = (props) => {
+  return <div className={props.color}></div>
+}
+
+// using object deconstruciton
+// // const Color = ({color}) => {
+//   return <div className={color}></div>
+// }
+
 const App = () => {
   return (
     <div id="container">
@@ -7,7 +16,11 @@ const App = () => {
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="red" />
+        <Color color="green" />
+        <Color color="blue" />
+      </div>
     </div>
   );
 };
